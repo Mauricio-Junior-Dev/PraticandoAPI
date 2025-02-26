4f01d95d3da507cb5497a7ea81a9295283ccfcd5
MauMau221 <mauriciobgsp@gmail.com>
2025-02-26 01:13:41 -0300

    Gerado link para apresentação da tela de login com google

---
+++ API_google_login/public/index.php
@@ -0,0 +1,33 @@
+<?php
+
+use app\library\GoogleClient;
+
+require '../vendor/autoload.php';
+
+
+$googleClient = new GoogleClient;
+$googleClient->init();
+
+$authUrl = $googleClient->generateAuthLink();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+    <input type="text" name="email" placeholder="Email">
+    <input type="text" name="password" placeholder="Password">
+    <button type="submit">Login</button>
+    <a href="<?php echo $authUrl; ?>">Login Google</a>
+
+    </form>    
+
+
+</body>
+</html>
